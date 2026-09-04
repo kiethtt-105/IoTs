8b223176ff48367f1e669678ce8a20af9517fa3e
Huỳnh Trần Tuấn Kiệt <kiethtt.105204@gmail.com>
2026-09-04 19:42:58 +0700

    DEL

--- backend/prisma/seed.ts
+++
@@ -1,9 +0,0 @@
-await prisma.user.upsert({
-  where: { email: process.env.ADMIN_EMAIL },
-  update: {},
-  create: {
-    email: process.env.ADMIN_EMAIL,
-    password: await hash(process.env.ADMIN_PASSWORD),
-    role: "ADMIN",
-  },
-});
